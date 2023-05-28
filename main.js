8935fce87c7ad1682e1c1f3845a003643c2de6c7
Султан <carameleclipsimus@gmail.com>
2023-05-28 17:31:02 +0600

    refactor: add modules

---
+++ main.js
@@ -0,0 +1,9 @@
+import {
+  remainingMinutesInOven,
+  preparationTimeInMinutes,
+  totalTimeInMinutes,
+} from "./modules/get-value.js";
+
+console.log(remainingMinutesInOven(25));
+console.log(preparationTimeInMinutes(10));
+console.log(totalTimeInMinutes(30, 20));
